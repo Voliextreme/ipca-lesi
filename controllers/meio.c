76e0f6b2170387374291f5bbc2dac693929cc9b3
Tiago Sampaio <80679321+Voliextreme@users.noreply.github.com>
2023-03-17 14:44:30 +0000

    Controladores para a struct meios e as funcoes necessarias a utilizar

---
+++ controllers/meio.c
@@ -0,0 +1,124 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "meio.h"
+
+
+int guardarMeios(Meio *inicio)
+{
+    FILE *fp;
+    fp = fopen("meios.txt", "w");
+    if (fp != NULL)
+    {
+        Meio *aux = inicio;
+        while (aux != NULL)
+        {
+            fprintf(fp, "%d;%f;%f;%s\n", aux->codigo, aux->bateria,
+                    aux->autonomia, aux->tipo);
+            aux = aux->seguinte;
+        }
+        fclose(fp);
+        return (1);
+    }
+    else
+        return (0);
+}
+
+Meio *lerMeios()
+{
+    FILE *fp;
+    int cod;
+    float bat, aut, custo;
+    char tipo[50], geoCodigo[50];
+    bool disponivel;
+    Meio *aux = NULL;
+    fp = fopen("meios.txt", "r");
+    if (fp != NULL)
+    {
+        while (!feof(fp))
+        {
+            fscanf(fp, "%d;%f;%f;%[^\n]s;%f;%[^\n]s;%\n", &cod, &bat, &aut, tipo, &custo, geoCodigo, &disponivel);
+            aux = inserirMeio(aux, cod, tipo, bat, aut, custo, geoCodigo, disponivel);
+        }
+        fclose(fp);
+    }
+    return (aux);
+}
+
+// Inserção de um novo registo
+Meio *inserirMeio(Meio *inicio, int codigo, char tipo[], float bateria, float autonomia, float custo, char geoCodigo[], bool disponivel)
+{
+    if (!existeMeio(inicio, codigo))
+    {
+        Meio *novo = malloc(sizeof(struct registo));
+        if (novo != NULL)
+        {
+            novo->codigo = codigo;
+            strcpy(novo->tipo, tipo);
+            novo->bateria = bateria;
+            novo->autonomia = autonomia;
+            novo->custo = custo;
+            strcpy(novo->geoCodigo, geoCodigo);
+            novo->disponivel = true;
+            novo->seguinte = inicio;
+            return (novo);
+        }
+    }
+    return (inicio);
+}
+
+// listar na consola o conteúdo da lista ligada
+void listarMeios(Meio *inicio)
+{
+    while (inicio != NULL)
+    {
+        printf("%d %s %.2f %.2f %.2f %s %d\n", inicio->codigo, inicio->tipo,
+               inicio->bateria, inicio->autonomia, inicio->custo, inicio->geoCodigo, inicio->disponivel);
+        inicio = inicio->seguinte;
+    }
+}
+
+// Determinar existência do 'codigo' na lista ligada 'inicio'
+// devolve 1 se existir ou 0 caso contrário
+int existeMeio(Meio *inicio, int cod)
+{
+    while (inicio != NULL)
+    {
+        if (inicio->codigo == cod)
+            return (1);
+        inicio = inicio->seguinte;
+    }
+    return (0);
+}
+
+// Remover um meio a partir do seu código{
+Meio *removerMeio(Meio *inicio, int cod)
+{
+    Meio *anterior = inicio, *atual = inicio, *aux;
+
+    if (atual == NULL)
+        return (NULL);
+    else if (atual->codigo == cod) // remoção do 1º registo
+    {
+        aux = atual->seguinte;
+        free(atual);
+        return (aux);
+    }
+    else
+    {
+        while ((atual != NULL) && (atual->codigo != cod))
+        {
+            anterior = atual;
+            atual = atual->seguinte;
+        }
+        if (atual == NULL)
+            return (inicio);
+        else
+        {
+            anterior->seguinte = atual->seguinte;
+            free(atual);
+            return (inicio);
+        }
+    }
+}
---
+++ controllers/meio.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef struct registo
+{int codigo; // código do meio de mobilidade elétrica
+ char tipo[50];
+ float bateria;
+ float autonomia;
+ float custo;
+ char geoCodigo[20];
+ bool disponivel;
+ struct registo* seguinte;
+} Meio;
+
+
+Meio* inserirMeio(Meio* inicio, int codigo, char tipo[], float bateria, float autonomia, float custo, char geoCodigo[], bool disponivel); // Inserção de um novo registo
+void listarMeios(Meio* inicio); // listar na consola o conteúdo da lista ligada
+Meio* removerMeio(Meio* inicio, int cod); // Remover um meio a partir do seu código
+int existeMeio(Meio* inicio, int codigo); // Determinar existência do 'codigo' na lista ligada 'inicio'
+ 
+
+int guardarMeios(Meio* inicio);
+Meio* lerMeios();
